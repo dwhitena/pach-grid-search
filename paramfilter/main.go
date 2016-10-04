7214696aa010c29cc2eb39bf63c67e722aadcc52
Daniel Whitenack <whitenack.daniel@gmail.com>
2016-10-04 09:50:36 -0400

    modified to generate combinations

--- paramfilter/main.go
+++ paramfilter/main.go
@@ -1,36 +1,82 @@
 package main
 
 import (
-	"encoding/csv"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
-	"strconv"
+	"strings"
 )
+
+// paramRange include a range for a parameter of a
+// certain name.
+type paramRange struct {
+	Name      string  `json:"name"`
+	Min       float64 `json:"min"`
+	Max       float64 `json:"max"`
+	Increment float64 `json:"increment"`
+}
+
+// paramRange is a slice of paramRange values.
+type paramRanges []paramRange
 
 func main() {
 
-	csvfile, err := os.Open("/pfs/repodata/repodata.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer csvfile.Close()
-
-	reader := csv.NewReader(csvfile)
-	reader.FieldsPerRecord = -1
-	rawCSVdata, err := reader.ReadAll()
+	// Read in the parameter file.
+	//file, e := ioutil.ReadFile("/pfs/parameters/parameters.json")
+	file, err := ioutil.ReadFile("example_parameters.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	count := 0
-	for _, each := range rawCSVdata {
-		intString := strconv.Itoa(count)
-		d1 := []byte(each[0])
-		err = ioutil.WriteFile("/pfs/out/"+intString, d1, 0644)
-		if err != nil {
-			log.Fatal(err)
+	// Unmarshal the ranges.
+	var pRanges paramRanges
+	if err := json.Unmarshal(file, &pRanges); err != nil {
+		log.Fatal(err)
+	}
+
+	// Prepare combinations.
+	var sets [][]float64
+	for _, rng := range pRanges {
+		set := []float64{}
+		currentVal := rng.Min
+		for currentVal <= rng.Max {
+			set = append(set, currentVal)
+			currentVal += rng.Increment
 		}
-		count++
+		sets = append(sets, set)
+	}
+
+	// Expand the combinations.
+	combos := make(map[string]string)
+	current := make([]float64, len(pRanges))
+	expand(sets, current, 0, combos)
+
+	// Gather and output the results.
+	for k, v := range combos {
+		fmt.Println(k, v)
+		//data := []byte(v)
+		//err = ioutil.WriteFile("/pfs/out/"+k, data, 0644)
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+	}
+}
+
+// expand computes the cartesian product of N float slices.
+func expand(input [][]float64, current []float64, k int, combos map[string]string) {
+	if k == len(input) {
+		var combo string
+		for i := 0; i < k; i++ {
+			combo = combo + fmt.Sprintf("%.2f ", current[i])
+		}
+		fileName := strings.Replace(combo, " ", "", -1)
+		fileName = strings.Replace(fileName, ".", "", -1)
+		combos[fileName] = combo
+	} else {
+		for i := 0; i < len(input[k]); i++ {
+			current[k] = input[k][i]
+			expand(input, current, k+1, combos)
+		}
 	}
 }
